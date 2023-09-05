9b8346523716aa6f0394f7f365b5145f5b60fda6
Sravani Gopavarapu <g.sravani57@gmail.com>
2023-09-05 13:11:33 +0200

    modified values

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Hello, Welcome to the shell scripting"
-per1=sravani
-per2=pavani
+per1=$1
+per2=$2
 
 echo "$per1 : How are you $per2"
 echo "$per2 : I am good Thank you how are you $per1"
