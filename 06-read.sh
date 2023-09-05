eaf19a302516eb194774ff897d36ef4cf8116863
Sravani Gopavarapu <g.sravani57@gmail.com>
2023-09-05 13:36:28 +0200

    added new file

--- 06-read.sh
+++ 06-read.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "please enter your user name"
-read -s USER
+read  USER #-s is useed to not echo the input data but somehow it is not working in ubuntu server
 echo "Please enter your password"
-read -s PASSWORD
+read  PASSWORD
 echo "The entered values : $USER and $PASSWORD"
---
+++ 07-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CITIES=("Hyderabad","Delhi","Mumbai")
+
+echo "First City is ${CITIES[0]}"
+echo "Second City is ${CITIES[1]}"
+echo "Third City is ${CITIES[2]}"
