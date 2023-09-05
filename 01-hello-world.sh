ab485749f1b14a398af850af11acc5472e30afe9
root <root@Marshall.localdomain>
2023-09-05 10:46:33 +0200

    updated variables

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Hello, Welsome to the shell scripting"
+echo "Hello, Welcome to the shell scripting"
+per1=sravani
+per2=pavani
+
+echo "$per1 : How are you $per2"
+echo "$per2 : I am good Thank you how are you $per1"
